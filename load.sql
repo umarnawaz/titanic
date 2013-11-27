9d973333c5c989695621694cce825f2205734f1b
Umar Nawaz <nawaz.umar@gmail.com>
2013-11-27 15:40:17 -0500

    first commit

---
+++ load.sql
@@ -0,0 +1,36 @@
+set search_path to kaggle_titanic;
+
+drop table raw_train;
+drop table raw_test;
+
+create table raw_train(
+passenger_id text,
+survived text,
+pclass text,
+name text,
+sex text,
+age text,
+sibsp text,
+parch text,
+ticket text,
+fare text,
+cabin text,
+embarked text
+);
+
+create table raw_test(
+passenger_id text,
+pclass text,
+name text,
+sex text,
+age text,
+sibsp text,
+parch text,
+ticket text,
+fare text,
+cabin text,
+embarked text
+);
+
+\copy raw_train from 'backup/train.csv' csv header
+\copy raw_test from 'backup/test.csv' csv header
